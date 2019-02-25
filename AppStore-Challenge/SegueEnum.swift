0aec04fe5eb94b5d354ed2b8460175aed720bcc7
Hubert Wang <hubert.tatra@gmail.com>
2019-02-25 22:32:02 +0700

    Add segue + enum helper file

---
+++ AppStore-Challenge/SegueEnum.swift
@@ -0,0 +1,14 @@
+//
+//  SegueEnum.swift
+//  AppStore-Challenge
+//
+//  Created by Hubert Wang on 25/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum NavigationDestinationEnum: String {
+    case appStoreVC = "navigateToAppStore"
+}
